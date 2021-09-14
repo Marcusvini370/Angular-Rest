4f04e9abbf9baa68a31dd0b890ed381bdde6733d
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2021-09-14 19:03:41 -0300

    Criando Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,29 @@
-
-import { Component } from '@angular/core';
-
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {  //toda ves que esse componente for iniciado o ngOinit vai ser executado
+
   title = 'angular-rest';
 
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {
+    if (localStorage.getItem('token') == null) { //se o token for inválido sera redirecionado para o login
+      this.router.navigate(['login']);
+    }
+  }
+
+  public sair(){
+    localStorage.clear();
+    this.router.navigate(['login']);
+  }
 
 }
+
+
+
