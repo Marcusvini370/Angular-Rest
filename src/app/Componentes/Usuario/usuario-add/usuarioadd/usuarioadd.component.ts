1ed7ff1d1faf8ac1dc51a4905a7686fcf49f7fe6
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2021-09-16 19:17:44 -0300

    Método de cadastrar e atualizar

--- src/app/Componentes/Usuario/usuario-add/usuarioadd/usuarioadd.component.ts
+++ src/app/Componentes/Usuario/usuario-add/usuarioadd/usuarioadd.component.ts
@@ -1,5 +1,7 @@
+import { User } from './../../../../model/user';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute} from '@angular/router';
+import { UsuarioService } from 'src/app/service/usuario.service';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +10,47 @@ import { ActivatedRoute} from '@angular/router';
 })
 export class UsuarioaddComponent implements OnInit {
 
-  constructor(private routerActive : ActivatedRoute) { }
+  usuario = new User();
+
+
+  constructor(private routerActive : ActivatedRoute,  private userService: UsuarioService) { }
 
 
   ngOnInit(): void {
-    let id = this.routerActive.snapshot.paramMap.get('id');
+     //vai pegar o id que está editando
+   let id = this.routerActive.snapshot.paramMap.get('id');
 
     if (id != null){
-        console.log(' Valor sendo editado : ' + id);
+      this.userService.getUsuario(id).subscribe(data => {
+        console.log(data);
+        this.usuario.id = data.id;
+        this.usuario.login = data.userLogin;
+        this.usuario.nome = data.userNome;
+        this.usuario.cpf = data.userCpf;
+        this.usuario.senha = data.senha;
+    });
+    }
+    }
+
+    salvarUser(){
+          if(this.usuario.id != null && this.usuario.id.toString().trim() != null){ /* Atuzalizando ou editando se o usuário existir*/
+              this.userService.updateUsuario(this.usuario).subscribe(data => {
+                  console.info("User Atualizado" + data);
+                  this.novo();
+              });
+
+          }else{ //salvando
+            this.userService.salvarUsuario(this.usuario).subscribe(data =>{
+              this.novo();
+              console.info("chamou save");
+            });
+          }
+    }
+
+    novo(){
+      this.usuario = new User();
     }
 
   }
 
-}
+
